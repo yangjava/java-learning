ddbc484db595a4393e87b6b0b4156b83bc6a774e
Jack Yang <yangjava@users.noreply.github.com>
2017-09-05 12:05:16 +0800

    zookeeper

--- zookeeper-learning/com/java/zookeeper/ZookeeperTest.java
+++
@@ -1,9 +0,0 @@
-package com.java.zookeeper;
-
-import org.apache.zookeeper.ZooKeeper;
-
-public class ZookeeperTest {
-	public static void main(String[] args) {
-		ZooKeeper zookeeper=new ZooKeeper(connectString, sessionTimeout, watcher);
-	}
-}
---
+++ zookeeper-learning/src/main/java/com/zookeeper/test/ZookeeperTest.java
@@ -0,0 +1,62 @@
+package com.zookeeper.test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Test;
+
+public class ZookeeperTest {
+	@Test
+	public void testZk1() throws Exception {
+		ZooKeeper zk = new ZooKeeper("127.0.0.1:2180", 60000, new Watcher() { // 监控所有被触发的事件
+					// //
+					// 当对目录节点监控状态打开时，一旦目录节点的状态发生变化，Watcher 对象的 process 方法就会被调用。
+					public void process(WatchedEvent event) {
+						System.out.println("EVENT:" + event.getType());
+					}
+
+				});
+		System.out.println(zk);
+		System.out.println("ls / => " + zk.getChildren("/", true));
+	}
+	@Test
+	public void testZk2() throws Exception{
+		ZooKeeper zk = new ZooKeeper("127.0.0.1:2180", 5000, new Watcher() {
+			CountDownLatch down = new CountDownLatch(1);// 同步阻塞状态
+
+			@Override
+			public void process(WatchedEvent event) {
+				if (event.getState() == Event.KeeperState.SyncConnected) {
+					down.countDown();// 连接上之后，释放计数器
+				}
+
+			}
+		});
+
+		System.out.println("连接成功：" + zk.toString());
+		System.out.println("ls / => " + zk.getChildren("/", true));
+//		zk.close();// 关闭连接
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ZooKeeper zk = new ZooKeeper("127.0.0.1:2180", 5000, new Watcher() {
+			CountDownLatch down = new CountDownLatch(1);// 同步阻塞状态
+
+			@Override
+			public void process(WatchedEvent event) {
+				if (event.getState() == Event.KeeperState.SyncConnected) {
+					down.countDown();// 连接上之后，释放计数器
+				}
+
+			}
+		});
+
+		System.out.println("连接成功：" + zk.toString());
+		System.out.println("ls / => " + zk.getChildren("/", true));
+	}
+
+
+}
